fc230631677b033ea26adaf1f47f268ea97cfb06
Vitalii <job.corsar@gmail.com>
2018-09-17 14:39:26 +0300

    Create View, create Controller

---
+++ PhoneBook_24/Controllers/ContactsController.cs
@@ -0,0 +1,40 @@
+﻿using PhoneBook_24.BusinessLogic.Models;
+using PhoneBook_24.BusinessLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phonebook24.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            var contacts = ContactService.GetAll();
+
+            return View(contacts);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Contact model)
+        {
+            ContactService.Create(model);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var contact = ContactService.Get(id);
+
+            return View();
+        }
+    }
+}
