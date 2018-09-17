d2b904a8cfead3c8f504f1fceaf5cbfebf56caa4
Vitalii <job.corsar@gmail.com>
2018-09-17 13:26:34 +0300

    Create repositoriy, create class Contact, create class Phohe

---
+++ PhoneBook_24.BusinessLogic/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook_24.BusinessLogic.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+
+        public List<Phone> Phones { get; set; }
+        public Contact()
+        {
+            Phones = new List<Phone>();
+        }
+        
+
+    }
+}
---
+++ PhoneBook_24.BusinessLogic/Services/ContactServices.cs
@@ -0,0 +1,89 @@
+﻿using PhoneBook_24.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook_24.BusinessLogic.Services
+{
+    interface IContactService
+    {
+        int Create(Contact contact);
+
+        void Update(Contact contact);
+
+        void Delete(Contact contact);
+
+        List<Contact> GetAll();
+
+        Contact Get(int id);
+    }
+
+    public static class ContactService
+    {
+        private static List<Contact> _contacts = new List<Contact>
+        {
+           new Contact
+           {
+               Email= "ads@djk.com",
+               Id =1,
+               Name = "Vitalii Che",
+               Phones = new List<Phone>
+               {
+                   new Phone
+                   {
+                       Type = 1,
+                       Number = "0501234567"
+                   }
+               }
+
+           }
+        };
+
+
+        public static int Create(Contact contact)
+        {
+            contact.Id = GetMax();
+
+            _contacts.Add(contact);
+
+            return contact.Id;
+        }
+
+
+        public static void Delete(int id)
+        {
+            var contact = Get(id);
+
+            if (contact != null)
+            {
+                _contacts.Remove(contact);
+            }
+        }
+
+        public static Contact Get(int id)
+        {
+            return _contacts.FirstOrDefault(x => x.Id == id);
+        }
+
+        public static List<Contact> GetAll()
+        {
+            return _contacts.OrderBy(x => x.Name).ToList();
+        }
+
+        public static void Update(Contact contact)
+        {
+            var oldContact = Get(contact.Id);
+
+            oldContact.Name = contact.Name;
+            oldContact.Email = contact.Email;
+            oldContact.Phones = contact.Phones;
+        }
+
+        private static int GetMax()
+        {
+            return _contacts.Max(x => x.Id) + 1;
+        }
+    }
+}
