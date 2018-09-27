2d37eac7cab4b9f24c6b561fbc1450bfb4e5a050
Vitalii <job.corsar@gmail.com>
2018-09-28 00:11:25 +0300

    Add Test Library

---
+++ TestLibrary/Models/CategoryLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary.Models
+{
+    public class CategoryLibrary
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Decription { get; set; }
+
+        public int CategoryLibraryId { get; set; }
+    }
+}
---
+++ TestLibrary/Models/TaskLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary.Models
+{
+    public class TaskLibrary
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
